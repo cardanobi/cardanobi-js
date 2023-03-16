9b8d89bcce103a4dc4061f821db91fd079a7b196
Thomas Langlois <thomas.langlois@mac.com>
2023-03-16 16:23:15 +0000

    pools.stats.epochs

--- domains/bi/pools.js
+++ domains/bi/pools.js
@@ -1,10 +1,37 @@
 'use strict'
 
 import { stats_ } from './pools/stats.js';
+import { handleError } from "../../utils/Misc.js";
+
+export async function epochs_(options) {
+    return new Promise((resolve, reject) => {
+        const { epoch_no, query, odata } = options || { undefined, undefined, undefined };
+        let path = `api/bi/pools/stats/epochs/${epoch_no}`;
+        if (query) path = path + "?" + query;
+
+        this.client.getPrivate(path)
+            .then(resp => {
+                resolve(resp);
+            })
+            .catch(err => {
+                // reject(handleError(err));
+                handleError(err);
+            });
+    });
+}
+
+export class Stats {
+    constructor(client) {
+        this.client = client;
+    }
+
+    epochs_ = epochs_;
+}
 
 export class Pools {
     constructor(client) {
         this.client = client;
+        this.stats = new Stats(this.client);
     }
 
     stats_ = stats_;
