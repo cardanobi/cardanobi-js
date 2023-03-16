3a7ad999ae7e749af28bc50c5df80ba310eed61d
Thomas Langlois <thomas.langlois@mac.com>
2023-03-16 14:47:36 +0000

    epochs latest, epochs params latest

--- domains/core/epochs.js
+++ domains/core/epochs.js
@@ -1,14 +1,33 @@
 'use strict'
 
-import { params_ } from './epochs/params.js';
+import { params_, Params } from './epochs/params.js';
 import { stakes_, Stakes } from './epochs/stakes.js';
+
+export async function latest_(options) {
+    return new Promise((resolve, reject) => {
+        const { query, odata } = options || { undefined, undefined };
+        let path = "api/core/epochs/latest";
+        if (query) path = path + "?" + query;
+
+        this.client.getPrivate(path)
+            .then(resp => {
+                resolve(resp);
+            })
+            .catch(err => {
+                // reject(handleError(err));
+                handleError(err);
+            });
+    });
+}
 
 export class Epochs {
     constructor(client) {
         this.client = client;
         this.stakes = new Stakes(this.client);
+        this.params = new Params(this.client);
     }
 
     params_ = params_;
     stakes_ = stakes_;
+    latest_ = latest_;
 }
--- domains/core/epochs/params.js
+++ domains/core/epochs/params.js
@@ -25,3 +25,28 @@ export async function params_(options) {
             });
     });
 }
+
+export async function latest_(options) {
+    return new Promise((resolve, reject) => {
+        const { query, odata } = options || { undefined, undefined };
+        let path = "api/core/epochs/params/latest";
+        if (query) path = path + "?" + query;
+
+        this.client.getPrivate(path)
+            .then(resp => {
+                resolve(resp);
+            })
+            .catch(err => {
+                // reject(handleError(err));
+                handleError(err);
+            });
+    });
+}
+
+export class Params {
+    constructor(client) {
+        this.client = client;
+    }
+
+    latest_ = latest_;
+}
